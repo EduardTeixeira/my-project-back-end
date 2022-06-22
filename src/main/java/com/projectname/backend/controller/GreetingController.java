aa980d197ef949267a697402f8a738544d75cf00
Eduard Teixeira <eduard.teixeira@mttechne.com.br>
2022-06-22 11:02:44 -0300

    configuração da controller

--- src/main/java/com/projectname/backend/controller/GreetingController.java
+++ src/main/java/com/projectname/backend/controller/GreetingController.java
@@ -1,40 +1,48 @@
 package com.projectname.backend.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projectname.backend.model.Greeting;
 
 @RestController
+@RequestMapping("/api/greeting")
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+	@GetMapping("")
+	public ResponseEntity<?> getAll() {
 
-	@GetMapping("/greeting")
-	public Greeting getGreeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		return new ResponseEntity<String>("Greeting - GET", HttpStatus.OK);
+
 	}
 
-	@PostMapping("/greeting")
-	public String postGreeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return "Greeting - POST";
+	@PostMapping("")
+	public ResponseEntity<?> create(@RequestBody Greeting request) {
+
+		return new ResponseEntity<String>("Greeting - POST", HttpStatus.OK);
+
 	}
 
-	@PutMapping("/greeting")
-	public String putGreeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return "Greeting - PUT";
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@PathVariable("id") Long id, @RequestBody Greeting request) {
+
+		return new ResponseEntity<String>("Greeting - PUT " + id, HttpStatus.OK);
+
 	}
 
-	@DeleteMapping("/greeting")
-	public String deleteGreeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return "Greeting - DELETE";
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteById(@PathVariable("id") Long id) {
+
+		return new ResponseEntity<String>("Greeting - DELETE " + id, HttpStatus.OK);
+
 	}
 
 }
